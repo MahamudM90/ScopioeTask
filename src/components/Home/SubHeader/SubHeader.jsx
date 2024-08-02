d90e225f26cdaec053b9c70e95428e291d04cf66
MahamudM90 <mahamud15-10467@diu.edu.bd>
2024-08-03 01:30:25 +0600

    Make flex item as per requriment

--- src/components/Home/SubHeader/SubHeader.jsx
+++ src/components/Home/SubHeader/SubHeader.jsx
@@ -2,27 +2,26 @@ import vector from '../../../assets/Vector.svg'
 import group from '../../../assets/Group.svg'
 export default function SubHeader() {
     return (
-        <div className='bg-[#FFFFFF] m-2 rounded'>
-            <div className='flex rounded p-2'>
-                <div className='w-[1130px] h-[212px] '>
+        <div className='bg-[#FFFFFF] m-2 rounded w-[1070px] h-[212px]'>
+            <div className='md:flex rounded p-4'>
+                <div className='w-[587px] h-[72px] '>
                     <h1 className='w-[587px] h-[14px] text-xl text-[#152A16]'>I'm Looking for Massage Therapist Near...</h1>
                     <br></br>
                     <p className='w-[489px] h-[33px] text-[#2E3439]'>In using this site, I agree to be bound by the <span className='underline text-[#156BCA]'>Terms of Service</span>  and  <span className='underline  text-[#156BCA]'>Privacy Policy</span></p>
-                    <br></br>
-                    <div className='flex items-center'>
-                        <div>
-                            <input type='text' placeholder='ZIP code or city name' className='border border-gray-300 rounded shadow-sm focus:outline-none w-[430px] h-[36px] bg-[#EEF2F5] p-2'></input>
-                        </div>
-                        <div>
-                            <button className='w-[83px] h-[40px] bg-[#156BCA] rounded -ml-1 text-[#FFFFFF]'>Go</button>
-                        </div>
-                    </div>
                 </div>
                 <div>
                     <img className='w-[351px] h-[172px]' src={vector}></img>
                 </div>
                 <div className='-ml-44'>
                     <img className=' w-[276.77px] h-[172px]' src={group}></img>
+                </div>
+            </div>
+            <div className='flex items-center -my-20 ml-3'>
+                <div>
+                    <input type='text' placeholder='ZIP code or city name' className='border border-gray-300 rounded shadow-sm focus:outline-none w-[430px] h-[36px] bg-[#EEF2F5] p-2'></input>
+                </div>
+                <div>
+                    <button className='w-[83px] h-[40px] bg-[#156BCA] rounded -ml-1 text-[#FFFFFF]'>Go</button>
                 </div>
             </div>
         </div>
