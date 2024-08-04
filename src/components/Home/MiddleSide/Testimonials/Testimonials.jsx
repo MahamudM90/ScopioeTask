46ee5b090b12f65c7a445b4d33ae87d2503c47cf
MahamudM90 <mahamud15-10467@diu.edu.bd>
2024-08-05 00:32:52 +0600

    done all

---
+++ src/components/Home/MiddleSide/Testimonials/Testimonials.jsx
@@ -0,0 +1,10 @@
+const Testimonials = () => {
+    return (
+        <div className="bg-primary rounded-[10px] p-5 w-full h-fit">
+            Test
+        </div>
+    );
+};
+
+export default Testimonials;
+
